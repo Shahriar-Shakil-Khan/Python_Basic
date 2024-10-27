effd332b17e2a251ae5bb81593d0c840a85ee58f
shahriar-shakil01797 <shahriarshakil28@gmail.com>
2024-10-27 23:14:56 +0600

    updated

---
+++ Py_array.py
@@ -0,0 +1,69 @@
+#Python does not have built-in support for Arrays, but Python Lists can be used instead.
+
+print()
+name=["Shakil","Pranto","Taki","Shawon"]
+
+print(name)
+
+print()
+
+for names in name:
+    print(names)
+    
+       
+print()   
+
+
+print()
+name=["BMW","FORD","VOLVO"]
+#name.pop(0)
+#name.pop(2)
+#name.remove("BMW")    #pop and remove are same 
+#name.append("Toyota")
+#name.clear()
+
+'''
+x=name.copy()
+print(x)
+'''
+
+'''
+x=name.count("FORD")
+print(x)
+'''
+
+'''
+x=len(name)
+print(x)
+'''
+
+'''
+aplabet=['d','c','a','b','e']
+aplabet.sort()
+print(aplabet)
+'''
+
+
+'''
+aplabet=['d','c','a','b','e']
+x=aplabet.index('b')
+print(x)
+'''
+
+'''
+fruits = [4, 55, 64, 32, 16, 32]
+
+x = fruits.index(32)
+
+print(x)
+'''
+
+'''
+fruits = ['apple', 'banana', 'cherry']
+
+fruits.insert(3,"orange")
+
+print(fruits)
+'''
+
+
