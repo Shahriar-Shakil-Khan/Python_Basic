561030a631eef1769271b9dbd288081cb253f28a
Shahriar Shakil <shahriarshakil28@gmail.com>
2025-02-01 23:44:19 +0600

    Update sum.py

--- sum.py
+++ sum.py
@@ -5,3 +5,20 @@ while(i<=n):
     sum=sum+i
     i=i+2
 print("Sum = " ,sum)
+
+
+n=int(input("Enter any number : = "))
+sum=0
+i=1
+while(i<=n):
+    sum=sum+i
+    i=i+1
+print("Sum = " ,sum)
+
+n=int(input("Enter any number : = "))
+sum=0
+i=1
+while(i<=n):
+    sum=sum+i
+    i=i+3
+print("Sum = " ,sum)
