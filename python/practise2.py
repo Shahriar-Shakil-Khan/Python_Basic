5aea13cd3c7f514d9ef16f7fb52bee57c026e887
Shahriar Shakil <shahriarshakil28@gmail.com>
2025-02-16 21:20:02 +0600

    Add files via upload

--- python/practise2.py
+++ python/practise2.py
@@ -1,26 +1,19 @@
-number_of_word = 0
-number_of_letter = 0
-number_of_digit = 0
+#python variable
+'''
+x=5 #3.5 float
+y="Shakil"
 
-text= input("Please!enter text : ")
+print(y)
+print(x)
 
-for number in text:
-    number=number.lower()
-    if number>="a" and number<="z":
-       number_of_letter = number_of_letter +1
-       
-    
-    elif number>="0" and number<="9":
-        number_of_digit = number_of_digit +1
-       
-    
-    elif number==" ":
-       number_of_word = number_of_word +1
-   
+print(type(y))
+print(type(x))
+'''
+x=5
+y=3.1215
+z=10
 
-print(number_of_word+1)
-print(number_of_letter)
-print(number_of_digit)
-       
-    
+print(float(x))
+print(int(y))
+print(str(5))
 
