46083e4b31b839bb0a55751b3532db936e8218ce
Shahriar Shakil <shahriarshakil28@gmail.com>
2025-02-17 20:23:07 +0600

    Add files via upload

---
+++ python/practise8.py
@@ -0,0 +1,60 @@
+
+x=["E","D","C","A","B"]
+x.sort()
+print(x)
+
+
+
+a = [100, 50, 65, 82, 23]
+a.sort()
+print(a)
+
+
+a = [100, 50, 65, 82, 23]
+x=a.copy()
+print(x)
+
+
+a = [100, 50, 65, 82, 23]
+x=list(a)
+print(x)
+
+
+l1=["a","b","c"]
+l2=[1,2,3]
+l3=l1+l2
+
+print(l3)
+
+l1=["a","b","c"]
+l2=[1,2,3]
+
+for x in l2:
+    l1.append(x)
+
+print(l1)
+
+
+l1=["a","b","c"]
+l2=[1,2,3]
+l1.extend(l2)
+print(l1)
+
+
+
+l1=["a","b","c"]
+l1.reverse()
+print(l1)
+
+
+
+
+
+
+
+
+
+
+
+
+
