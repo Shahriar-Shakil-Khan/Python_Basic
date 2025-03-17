7721abeb83f02bf615390002b67f8d02701c3ce1
Shahriar   Shakil <shahriarshakil28@gmail.com>
2025-03-17 20:53:09 +0600

    Add files via upload

---
+++ 5_student_marks_grade.py
@@ -0,0 +1,28 @@
+num = float(input("Enter number As you wish : "))
+
+if num >= 90:
+    print("you get A+")
+
+elif num >= 85:
+    print("you get A")
+
+elif num >= 80:
+    print("you get B+")
+
+elif num >= 75:
+    print("you get B")
+
+elif num >= 70:
+    print("you get C+")
+
+elif num >= 65:
+    print("you get C")
+
+elif num >= 60:
+    print("you get D+")
+
+elif num >= 50:
+    print("you get D")
+
+else:
+    print("Fail")
