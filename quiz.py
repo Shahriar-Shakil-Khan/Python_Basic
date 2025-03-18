ce7e0ab59da896750eee2dd518fe5ecf4e519abb
shahriar-shakil01797 <shahriarshakil28@gmail.com>
2025-03-18 22:38:01 +0600

    shakil

--- quiz.py
+++ quiz.py
@@ -8,8 +8,33 @@ x="Hello, world"
 a=x.upper()
 print(a)
 
-'''
+
 
 x="SHAHRIAR IBNE AMIN SHAKIL"
 a=x.lower()
+print(a)
+
+
+
+x=" Hello World "
+print(x)
+
+a=x.strip()
+print(a)
+
+
+
+x="Hello, Shakil"
+a=x.replace("o","k")
+print(a)
+
+
+
+x="Hello, Sabil"
+print(x.replace("S","N"))
+
+'''
+
+x="Shahriar , Ibne , Amin , Shakil"
+a=x.split(",")
 print(a)
